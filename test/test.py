fae3ebef35fe063d3314c76de3109d60ad1a0441
1151609563@qq.com <13671383052nj>
2025-02-06 22:18:20 +0800

    修改测试案例

--- test/test.py
+++ test/test.py
@@ -8,13 +8,35 @@ def maximum():
     print(res1)
     print(res)
 
-maximum()
+#maximum()
 
 def test_mean():
     """mean求的平均，会根据传入参数的第一个维度大小来计算"""
     a = np.array([[2], [2]])
     b = np.array([[3], [3]])
-    r = np.mean(a - b)
+    
+    # 默认第一维度计算
+    r1 = np.mean(a-b)
+
+    # 也可以指定维度计算
+    r = np.mean(a - b, -1)
     print(r)
 
-test_mean()
+#test_mean()
+
+def matrix_calcu():
+    a = np.array([[1, 1, 1],[2, 2, 2]])
+
+    b = np.array([[3, 3, 3],[4, 4, 4]])
+    b_T = b.T
+
+    print(a.shape)
+    print(b_T.shape)
+    
+    print(a)
+    print(b_T)
+    d = np.dot(a, b_T)
+    print(d)
+
+
+matrix_calcu()
